48639e5a8d5cf189be98367af2a6804f17c637c1
ColleenCallonego <colleencallonego@gmail.com>
2020-03-31 22:38:29 +0200

    écriture du code du generer_afffectation()

--- compilation/src/main/java/fr/ul/miage/coco/compilation/Main.java
+++ compilation/src/main/java/fr/ul/miage/coco/compilation/Main.java
@@ -10,14 +10,14 @@ public class Main {
 		
 	}
 	
-	public static String generer_programme(Prog a, Tds t) {
+	public static String generer_Noeudramme(Noeud a, Tds t) {
 		String newLigne = System.getProperty("line.separator");
 		String res;
 		res = "";
 		res += ".include.uasm" + newLigne + ".include intio.uasm" + newLigne + ".options tty" + newLigne + "CMOVE(pile, sp)" + newLigne + "BR(debut)";
-		res += generer_data(t);
-		res += generer_code(a);
-		res += "debut :" + newLigne + "CALL(main)" + newLigne + "HALT()" + newLigne + "pile :";
+		res += newLigne + generer_data(t);
+		res += newLigne + generer_code(a);
+		res += newLigne + "debut :" + newLigne + "CALL(main)" + newLigne + "HALT()" + newLigne + "pile :";
 		return res;
 	}
 	
@@ -25,35 +25,40 @@ public class Main {
 		return "a faire";
 	}
 	
-	public static String generer_code(Prog a) {
+	public static String generer_code(Noeud a) {
 		return "a faire";
 	}
 	
-	public static String generer_expression(Prog a, Tds t) {
+	public static String generer_expression(Noeud a, Tds t) {
 		return "a faire";
 	}
 	
-	public static String generer_affectation(Prog a, Tds t) {
+	public static String generer_affectation(Noeud a, Tds t) {
+		String newLigne = System.getProperty("line.separator");
+		String res;
+		res = "";
+		res += generer_expression(a.getFils().get(0), t);
+		res += newLigne + "POP(R0)" + newLigne + "ST(R0, " + a.getFils().get(0).getLabel() + ")";
+		return res;
+	}
+	
+	public static String generer_instruction(Noeud a, Tds t) {
 		return "a faire";
 	}
 	
-	public static String generer_instruction(Prog a, Tds t) {
+	public static String generer_ecrire(Noeud a, Tds t) {
 		return "a faire";
 	}
 	
-	public static String generer_ecrire(Prog a, Tds t) {
+	public static String generer_si(Noeud a, Tds t) {
 		return "a faire";
 	}
 	
-	public static String generer_si(Prog a, Tds t) {
+	public static String generer_bloc(Noeud a, Tds t) {
 		return "a faire";
 	}
 	
-	public static String generer_bloc(Prog a, Tds t) {
-		return "a faire";
-	}
-	
-	public static String generer_expression_boolean(Prog a, Tds t) {
+	public static String generer_expression_boolean(Noeud a, Tds t) {
 		return "a faire";
 	}
 }
