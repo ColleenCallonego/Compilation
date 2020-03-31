f7b62101db68dece0bd96430c3c95e6721f73c09
ColleenCallonego <colleencallonego@gmail.com>
2020-03-31 21:57:22 +0200

    ecriture du code de generer_programme() qui semble marcher :D

--- compilation/src/main/java/fr/ul/miage/coco/compilation/Main.java
+++ compilation/src/main/java/fr/ul/miage/coco/compilation/Main.java
@@ -1,5 +1,33 @@
 package fr.ul.miage.coco.compilation;
 
+import java.util.logging.Logger;
+
+import fr.ul.miage.arbre.*;
+import fr.ul.miage.tds.*;
+
 public class Main {
-	//J'arrive à rien putain
+	private static final Logger LOG = Logger.getLogger(Main.class.getName());
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static String generer_programme(Prog a, Tds t) {
+		String newLigne = System.getProperty("line.separator");
+		String res;
+		res = "";
+		res += ".include.uasm" + newLigne + ".include intio.uasm" + newLigne + ".options tty" + newLigne + "CMOVE(pile, sp)" + newLigne + "BR(debut)";
+		res += generer_data(t);
+		res += generer_code(a);
+		res += "debut :" + newLigne + "CALL(main)" + newLigne + "HALT()" + newLigne + "pile :";
+		return res;
+	}
+	
+	public static String generer_data(Tds t) {
+		return "a faire";
+	}
+	
+	public static String generer_code(Prog a) {
+		return "a faire";
+	}
 }
