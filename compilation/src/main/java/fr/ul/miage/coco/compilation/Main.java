2e0487ed652c8e178e2a680053cff014b9e352e6
Vellucen <gabriel.couroux33@orange.fr>
2020-04-04 18:58:50 +0200

    Algo gaby

--- compilation/src/main/java/fr/ul/miage/coco/compilation/Main.java
+++ compilation/src/main/java/fr/ul/miage/coco/compilation/Main.java
@@ -39,7 +39,7 @@ public class Main {
 		res += ".include.uasm" + newLigne + ".include intio.uasm" + newLigne + ".options tty" + newLigne + "CMOVE(pile, sp)" + newLigne + "BR(debut)";
 		res += newLigne + generer_data(t);
 		res += newLigne + generer_code(a);
-		res += newLigne + "debut :" + newLigne + "CALL(main)" + newLigne + "HALT()" + newLigne + "pile :";
+		res += newLigne + "debut :" + newLigne + "CALL(main)" + newLigne + "HALT()" + newLigne + "pile :" + newLigne;
 		return res;
 	}
 	
@@ -52,14 +52,68 @@ public class Main {
 	}
 	
 	public static String generer_expression(Noeud a, Tds t) {
-		return "a faire";
+		String res;
+		res = "";
+		switch(a.getCat()) {
+		/*La racine de l'arbre en paramètre est une constante*/
+		case CONST:
+			res += "CMOVE("+a.getLabel()+", R0)" + newLigne + 
+				   "PUSH(R0)";
+			break;
+		/*La racine de l'arbre en paramètre est un identifient*/
+		case IDF:
+			res += "LD("+a.getLabel()+", R0)" + newLigne + 
+				   "PUSH(R0)";
+			break;
+		/*La racine de l'arbre en paramètre est une opération : ADDITION*/
+		case PLUS:
+			res += generer_expression(a.getFils().get(0), t) + newLigne;
+			res += generer_expression(a.getFils().get(1), t) + newLigne;
+			res += "POP(R2)" + newLigne +
+				   "POP(R1)" + newLigne +
+				   "ADD(R1, R2, R3)" + newLigne +
+				   "PUSH(R3)" + newLigne;
+			break;
+		/*La racine de l'arbre en paramètre est une opération : SOUSTRACTION*/
+		case MOINS:
+			res += generer_expression(a.getFils().get(0), t) + newLigne;
+			res += generer_expression(a.getFils().get(1), t) + newLigne;
+			res += "POP(R2)" + newLigne +
+				   "POP(R1)" + newLigne +
+				   "SUB(R1, R2, R3)" + newLigne +
+				   "PUSH(R3)" + newLigne;
+			break;
+		/*La racine de l'arbre en paramètre est une opération : MULTIPLICATION*/
+		case MUL:
+			res += generer_expression(a.getFils().get(0), t) + newLigne;
+			res += generer_expression(a.getFils().get(1), t) + newLigne;
+			res += "POP(R2)" + newLigne +
+				   "POP(R1)" + newLigne +
+				   "MUL(R1, R2, R3)" + newLigne +
+				   "PUSH(R3)" + newLigne;
+			break;
+		/*La racine de l'arbre en paramètre est une opération : DIVISION*/
+		case DIV:
+			res += generer_expression(a.getFils().get(0), t) + newLigne;
+			res += generer_expression(a.getFils().get(1), t) + newLigne;
+			res += "POP(R2)" + newLigne +
+				   "POP(R1)" + newLigne +
+				   "DIV(R1, R2, R3)" + newLigne +
+				   "PUSH(R3)" + newLigne;
+			break;
+		/*La racine de l'arbre en paramètre ne correspond à aucun t*/
+		default:
+			System.out.println("Erreur l'arbre ne correspond pas à une expression");
+			break;
+		}
+		return res;
 	}
 	
 	public static String generer_affectation(Noeud a, Tds t) {
 		String res;
 		res = "";
 		res += generer_expression(a.getFils().get(0), t);
-		res += newLigne + "POP(R0)" + newLigne + "ST(R0, " + a.getFils().get(0).getLabel() + ")";
+		res += newLigne + "POP(R0)" + newLigne + "ST(R0, " + a.getFils().get(0).getLabel() + ")" + newLigne;
 		return res;
 	}
 	
@@ -68,7 +122,20 @@ public class Main {
 	}
 	
 	public static String generer_ecrire(Noeud a, Tds t) {
-		return "a faire";
+		String res;
+		res = "";
+		res += generer_expression(a.getFils().get(0), t) + newLigne;
+		res += "POP(R0)" + newLigne + 
+			   "WRINT()" + newLigne;
+		return res;
+	}
+	
+	public static String generer_lire(Noeud a, Tds t) {
+		String res;
+		res = "";
+		res += "RDINT()" + newLigne +
+			   "PUSH(R0)";
+		return res;
 	}
 	
 	public static String generer_si(Noeud a, Tds t) {
